72d297b025ac1cf0a9b674e3bbe8b17d890abc5b
codeart007 <codeart007@gmail.com>
2020-02-04 04:54:37 -0800

    div tags added for make the content different

--- index.php
+++ index.php
@@ -3,8 +3,16 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" href="styles/master.css">
   </head>
   <body>
-    <h1>Welcome to ecommerce projet</h1>
+
+    <div class="">
+      <div class="">This is header area</div>
+      <div class="">This is menu bar</div>
+      <div class="">This is content area</div>
+      <div class="">This is side bar</div>
+      <div class="">This is footer</div>
+    </div>
   </body>
 </html>
